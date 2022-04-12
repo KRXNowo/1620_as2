4746ff5134a4d2fb49856430bcf0dae47a38bc5e
KRXN <kevangelista@my.bcit.ca>
2022-04-11 22:36:45 -0700

    added save a note task

--- src/app.js
+++ src/app.js
@@ -1,29 +1,61 @@
 const notes = [
-  { 
-    title: "first note", 
+  {
+    title: "first note",
     noteBody: "this is an example note",
-    id: 1 
+    id: 1,
+  },
+];
+
+//Add Text
+const addTextBtn = document.querySelector(".icons");
+
+newText = ` 
+<div id="text">
+    <textarea id="story" title="story"
+    rows="5" cols="40" placeholder="First line is the title"></textarea>
+<div id="optionbutton">
+    <button class="saveButton">save</button>
+    <button class="deleteButton">cancel</button>
+</div>  `;
+
+let saveTextBtn = null;
+let deleteTextBtn = null;
+
+function addNewText() {
+  const newTextArea = document.querySelector(".write-note-area");
+  newTextArea.insertAdjacentHTML("afterbegin", newText);
+  saveTextBtn = document.querySelector(".saveButton");
+  saveTextBtn.addEventListener("click", assemblePad);
+  deleteTextBtn = document.querySelector(".deleteButton");
+  deleteTextBtn.addEventListener("click", deleteText);
+  addTextBtn.removeEventListener("click", addNewText);
+}
+addTextBtn.addEventListener("click", addNewText);
+
+//Delete Text
+
+function deleteText() {
+  const Texts = document.querySelector(".write-note-area");
+  while (Texts.hasChildNodes()) {
+    Texts.removeChild(Texts.firstChild);
   }
-]
-//Add Note Area
-const add_note_btn = document.querySelector('.icons')
-const note_area = document.querySelector('.write-note-area')
-
-const add_note = `
-<div class="text-div">
-<label for="note">NOTE:</label>
-<textarea id="note-box" name="note" rows="20" cols="80" placeholder="Your Note Here..."></textarea>
-    <button id="save-button" type="button">
-        Save
-    </button>
-    <button id="delete-button" type="button">
-        Cancel
-    </button>
-`
-
-function addNote() {
-  note_area.insertAdjacentHTML("afterbegin", add_note)
-  add_note_btn.removeEventListener('click',addNote)
+  addTextBtn.addEventListener("click", addNewText);
 }
 
-add_note_btn.addEventListener('click', addNote)
+//Save Text
+
+const noteDisplayArea = document.querySelector(".notes-list");
+
+function newPad() {
+  const notePad = document.querySelector("#story");
+  const titles = notePad.value.split("\n")[0];
+  notes.push({ title: titles, noteBody: notePad.value, id: notes.length + 1 });
+  template = `  <li>${titles}</li>`;
+  noteDisplayArea.insertAdjacentHTML("beforeend", template);
+  console.log(notes);
+}
+
+function assemblePad() {
+  newPad();
+  deleteText();
+}
